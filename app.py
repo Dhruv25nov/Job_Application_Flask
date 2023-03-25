9cb4328c77e37ce4c0de0fc5d29f0b7c1b6c5f91
Dhruv Maheshwari <dhruv25nov@gmail.com>
2023-03-26 00:39:12 +0530

    Application Submission

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import get_jobs
+from flask import Flask, render_template, jsonify, request
+from database import get_jobs, get_job
 
 
 app = Flask(__name__)
@@ -8,11 +8,31 @@ app = Flask(__name__)
 company_name = "Jovian"
 jobs_list = get_jobs()
 
+# Home Page route
+
 
 @app.route("/")
-def hello():
+def list_all_jobs():
     return render_template("home.html", jobs=jobs_list, portal_name=company_name)
-    # print(type(jobs_list))
+
+
+# Specific job page route
+
+
+@app.route("/job/<id>")
+def list_job(id):
+    job = get_job(id)
+    if not job:
+        return "NOT FOUND! 404"
+    return render_template("jobpage.html", job=job)
+
+
+@app.route("/job/<id>/apply", methods=["POST"])
+def job_apply(id):
+    applicant_data = request.form
+    job_applied = get_job(id)
+   
+    return render_template("application_submitted.html", applicant=applicant_data, job = job_applied)
 
 
 @app.route("/api/jobs")
--- database.py
+++ database.py
@@ -4,13 +4,13 @@ from sqlalchemy import create_engine, text
 import pymysql
 from dotenv import load_dotenv
 import os
+import json
 
 # Load the environment variables from the .env file
 load_dotenv()
 
 # Get the password from the environment variable
 connection_str = os.environ.get('CONNECTION_STRING')
-
 
 
 ssl_args = {'ssl': {"ssl_cert": "/etc/ssl/cert.pem"}}
@@ -23,8 +23,25 @@ engine = create_engine(
 def get_jobs():
     with engine.connect() as conn:
         result = conn.execute(text("select * from jobs"))
+
         jobs = []
         for res in result.all():
             jobs.append(res._asdict())
-            # jobs.append(dict(res))
+
         return jobs
+
+
+def get_job(id):
+    with engine.connect() as conn:
+        query = f"select * from jobs where id={id} "
+        result = conn.execute(text(query))
+        # print(type(result.all()[0]))
+        
+        rows = result.all()
+        if len(rows):
+            return rows[0]._asdict()
+        else:
+            return None
+
+       
+
